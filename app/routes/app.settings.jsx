0fb8b0e618387d4209a2526efc11dae17e88b415
Lalit Thapa <lalithapaji@gmail.com>
2025-02-14 18:25:51 +0530

    Added Form Validation

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,72 +1,77 @@
-import {
-    Page,
-    FormLayout,
-    TextField,
-    LegacyStack,
-    Text,
-    InlineGrid,
-  } from "@shopify/polaris";
-import { useState } from "react";
-  
-  export default function Settings() {
-    const [storeName, setStoreName] = useState("");
-    const [accountEmail, setAccountEmail] = useState("");
-    return (
-      <Page>
-        <LegacyStack vertical>
-            <Text variant="heading2xl" as="h3">
-               Dashboard
-            </Text>
-        </LegacyStack>
 
-        <SpacingBackground>
-      <InlineGrid gap="400" columns={3}>
-        <Placeholder height="320px" />
-        <Placeholder height="320px" />
-        <Placeholder height="320px" />
-      </InlineGrid>
-    </SpacingBackground>
-        <FormLayout >
-            <TextField label="Store name"
-                    onChange={(value) => setStoreName(value)}
-                    autoComplete="off"
-                    value={storeName} />
-            <TextField
-                    type="email"
-                    label="Account email"
-                    onChange={(value) => setAccountEmail(value)}
-                    autoComplete="email"
-                    value={accountEmail}
-                />
-        </FormLayout>
-      </Page>
-    );
-  } 
-  const SpacingBackground = ({ children, width = '100%' }) => {
-    return (
-      <div
-        style={{
-          background: 'var(--p-color-bg-surface-success)',
-          width,
-          height: 'auto',
-        }}
-      >
-        {children}
-      </div>
-    );
+import { json, useLoaderData } from "@remix-run/react";
+import { BlockStack, Box, Button, Card, Form, InlineGrid, Page, Text, TextField } from "@shopify/polaris";
+import { useEffect, useState } from "react";
+
+//
+export async function loader(){
+const settings= {
+  name: "My Store",
+  description: "This is my store"
+}
+return json(settings);
+}
+
+export async function action(){
+alert("Data Saved");
+}
+
+export default function Settings() {
+  const settings  = useLoaderData();
+  const [isFormUpdated, setIsFormUpdated] = useState(false);
+  const [storeDetails, setStoreDetails] = useState(settings);
+
+  const updateForm = (key, value) => {
+    setStoreDetails({
+      ...storeDetails,
+      [key]: value
+    });
+    setIsFormUpdated(true);
   };
-  
-  const Placeholder = ({height = 'auto', width = 'auto'}) => {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          background: 'var(--p-color-bg-surface)',
-          height: height ?? undefined,
-          width: width ?? undefined,
-        }}
-      ><Text>Text</Text> </div>
-    );
-  };
+  useEffect(() => {
+    setIsFormUpdated(false);
+  }, [])
+
+
+  return (
+    <Page
+      divider
+      primaryAction={{ content: "View on your store", disabled: true }}
+      secondaryActions={[
+        {
+          content: "Duplicate",
+          accessibilityLabel: "Secondary action label",
+          onAction: () => alert("Duplicate action"),
+        },
+      ]}
+    >
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Store Name
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Your store name is the name of your store. It appears on your online store, invoices, and receipts.
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <Form method="POST">
+            <BlockStack gap="400">
+              <TextField label="Name" value={ storeDetails?.name } onChange={(value)=>updateForm('name',value)} />
+              <TextField label="Description" value={ storeDetails?.description }  onChange={(value)=>updateForm('description',value)} />
+                <Button submit={!isFormUpdated} disabled={!isFormUpdated} >Save Data</Button>
+            </BlockStack>
+           </Form>
+          </Card>
+        </InlineGrid>
+      </BlockStack>
+    </Page>
+  )
+}
