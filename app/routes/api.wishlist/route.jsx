ee5162f58362011f4302c6285e12f91db667ce05
Lalit Thapa <lalitthapa2108@gmail.com>
2025-02-20 00:52:16 +0530

    wishlist data submitted to DB

--- app/routes/api.wishlist/route.jsx
+++ app/routes/api.wishlist/route.jsx
@@ -1,15 +1,33 @@
-import { json } from '@remix-run/react'
+import { json } from '@remix-run/react';
+import db from '../../db.server';
+import { cors } from 'remix-utils/cors';
 
 export async function loader(){
-  const mainList = {message: 'Data Get successfully', method: "GET"};
+  const mainList = await db.wishlist.findMany({});
   return json(mainList);
 }
 
 export async function action({request}){
   const method = request.method;
+  let data = await request.formData(); 
+  data = Object.fromEntries(data); 
+  const customerId = data.customerId;
+  const productId = data.productId;
+  const shop = data. shop;
+  if(!customerId ||  !productId || !shop) {
+  return json({message: "Missing data. Required data: customerId, productId, shop", method: method});
+  }
   switch(method){
     case "POST":
-     return json({message: 'Data Post successfully', status: 200 , method: method});
+      let wishlist = await db.wishlist.create({
+        data: {
+          customerId,
+          productId,
+          shop
+        }
+      }); 
+     const response = json({message: 'Data Post successfully', status: 200 , method: method, wishlist:wishlist});
+     return cors(request,response)
      case "GET":
       return json({message: 'Data Get successfully', status: 200 , method: method});
      case "PUT":
