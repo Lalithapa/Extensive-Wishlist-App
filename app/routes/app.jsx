fee204f0bc2c24485ec811407b78f7bfb7703b03
Lalit Thapa <lalitthapa2108@gmail.com>
2025-03-11 01:03:42 +0530

    Fetch successfully on Backend App

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -22,6 +22,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
+        <Link to="/app/wishlist_items">All Wishlist</Link>
         <Link to="/app/settings">Settings</Link>
         <Link to="/app/additional">Additional page</Link>
       </NavMenu>
